732e92c4bf7979d739bc356adb84b0e2d1bde42d
Mary West <westmarye1219@gmail.com>
2020-02-29 09:54:38 -0600

    data added

---
+++ javascripts/helpers/data/personData.js
@@ -0,0 +1,135 @@
+persons = [
+{
+  id: 'person1',
+  name:'zoe',
+  isDead: 'false',
+},
+{
+  id: 'person2',
+  name:'luke',
+  isDead: 'false',
+},
+{
+  id: 'person3',
+  name:'mary',
+  isDead: 'false',
+},
+{
+  id: 'person4',
+  name:'sarah',
+  isDead: 'false',
+},
+{
+  id: 'person5',
+  name:'davis',
+  isDead: 'false',
+},
+{
+  id: 'person6',
+  name:'steve c',
+  isDead: 'false',
+},
+{
+  id: 'person7',
+  name:'beth',
+  isDead: 'false',
+},
+{
+  id: 'person8',
+  name:'monique',
+  isDead: 'false',
+},
+{
+  id: 'person9',
+  name:'dylan',
+  isDead: 'false',
+},
+{
+  id: 'person10',
+  name:'zach',
+  isDead: 'true',
+},
+{
+  id: 'person11',
+  name:'todd',
+  isDead: 'false',
+},
+{
+  id: 'person12',
+  name:'liza',
+  isDead: 'false',
+},
+{
+  id: 'person13',
+  name:'bethany',
+  isDead: 'false',
+},
+{
+  id: 'person14',
+  name:'john',
+  isDead: 'false',
+},
+{
+  id: 'person15',
+  name:'joey',
+  isDead: 'false',
+},
+{
+  id: 'person16',
+  name:'anca',
+  isDead: 'false',
+},
+{
+  id: 'person17',
+  name:'jose',
+  isDead: 'false',
+},
+{
+  id: 'person18',
+  name:'michelle',
+  isDead: 'false',
+},
+{
+  id: 'person19',
+  name:'aaron',
+  isDead: 'false',
+},
+{
+  id: 'person20',
+  name:'kayla',
+  isDead: 'false',
+},
+{
+  id: 'person21',
+  name:'chris',
+  isDead: 'false',
+},
+{
+  id: 'person22',
+  name:'ola',
+  isDead: 'false',
+},
+{
+  id: 'person23',
+  name:'steven',
+  isDead: 'false',
+},
+{
+  id: 'person24',
+  name:'kenny',
+  isDead: 'false',
+},
+{
+  id: 'person25',
+  name:'david',
+  isDead: 'false',
+}
+
+]
+
+
+const getAlivePersons = () => {
+
+}
+
+getAlivePersons();
